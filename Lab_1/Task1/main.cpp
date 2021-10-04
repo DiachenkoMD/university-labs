f2882f7c1186dd700806b70c74c33c9740841548
Nekear <diachenko_md@hotmail.com>
2021-10-04 20:53:16 +0300

    exp function added

--- Lab_1/Task1/main.cpp
+++ Lab_1/Task1/main.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 
 #define M_PI 3.14159265358979323846
-#define M_E 2.71828182845904523536
 
 using namespace std;
 
@@ -13,7 +12,7 @@ int main() {
     cin >> x >> a;
 
     double numerator = 2*cos((M_PI*x)/180) + sqrt(3)*sin((4*a*M_PI)/180)-1;
-    double denominator = 2*sin(pow((M_PI*a)/180, 2))+cos((M_PI*42)/180)-pow(M_E, pow(x, 1.0/3));
+    double denominator = 2*sin(pow((M_PI*a)/180, 2))+cos((M_PI*42)/180)-exp(pow(x, 1.0/3));
 
     a = numerator / denominator;
 
